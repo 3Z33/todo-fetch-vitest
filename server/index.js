7043f82aa4d88651968b7e139a917f971bacb8cb
hv3n <hv3n.dev@gmail.com>
2025-11-20 15:41:44 +0100

    fix: clean server code + robust CORS (Render compatible)

--- server/index.js
+++ server/index.js
@@ -27,8 +27,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
-}
-
+});
 
 // GET /api/tasks
 app.get('/api/tasks', async (req, res) => {
